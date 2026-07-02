62267fb8525268a5c41048a3f06f128a06b3a38e
Sushil Kumar <sushilkumarnk25102@gmail.com>
2026-07-02 18:51:39 +0530

    I have Created Flask project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/api")
+def api():
+    return jsonify({
+        "name": "Sushil Kumar",
+        "course": "B.tech"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
